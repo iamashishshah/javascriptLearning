183acdf40842c2d8e4885157fe7a192ab2cf38bf
Ashish Shah <133593891+iamashishshah@users.noreply.github.com>
2024-03-24 12:42:09 +0000

    Learnt some datatypes

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version
+/*
+
+bigInt
+string
+float
+number
+boolean
+null => it is a standlone value
+undefined => when nothing is defined 
+object
+symbol => unique
+*/
+let num = 33
+console.log(typeof undefined); // has output undefined only
+console.log(typeof null)  // it has object type
