194396fc22505fec1e76dedf4bef0f00cc753fdb
Ashish Shah <133593891+iamashishshah@users.noreply.github.com>
2024-03-24 12:25:33 +0000

    Learn about variables, where and when to use it.

---
+++ Basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 33323
+let name = "Ashish Shah"
+var accountPass = "Ashish@gmail.com"
+email = "tempmail@gmail.com"
+let accountState;
+
+/*
+we prefer not to use 'var' keyword
+cuz of block and function scope
+*/
+// accountId = 88888; not allowed
+name = "Abhay Sharma"
+accountPass = "thisisashishshah";
+email = "thisisnotamail";
+console.table([accountId, name, accountPass, email, accountState])
+
+
