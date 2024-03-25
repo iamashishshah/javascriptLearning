2ee8f424e889befda2eaf5beb641fdeee3e3dec4
Ashish Shah <133593891+iamashishshah@users.noreply.github.com>
2024-03-25 05:28:24 +0000

    Learning string

---
+++ Basics/05_string.js
@@ -0,0 +1,10 @@
+let myName = new String("This is Ashish Shah")  // this creates object
+let normalName = "Ashish Shah"
+console.log(myName)
+console.log(normalName)
+
+// ***********Fucntions for String********************
+
+console.log(myName.valueOf())   //Converts obj string into normal string
+let unTrimName = "           Ashish-shah        "
+console.log(unTrimName.trim())   
