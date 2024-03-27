b181e9887202c53fc67d313a097c83c7624bde93
Ashish Shah <133593891+iamashishshah@users.noreply.github.com>
2024-03-27 13:28:48 +0000

    scope

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -1,4 +1,11 @@
 let a = 22
 var b = 33
 const c = 433
+if (true) {
+    // let a = 50 here I am re-declaring the variable, that's why it value will not reflect outside of scope
+    a = 50
+    console.log(a)
+}
+
+console.table(a)
 
